7a7135447e741b746a3cb94ea71d3c7ef57f7cca
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-18 09:48:49 -0300

    Começando exercicios bloco 13.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.renderDog = this.renderDog.bind(this);
+    this.fetchDog = this.fetchDog.bind(this);
+    this.saveDog = this.saveDog.bind(this);
+    this.state = {
+      isLoading: true,
+      lastDog: '',
+      listOfDogs: [],
+    }
+  }
+
+  async fetchDog() {
+    this.setState({ isLoading: true }, async () => {
+      const endpoint = 'https://dog.ceo/api/breed/shihtzu/images/random';
+      const requestReturn = await fetch(endpoint);
+      const requestResponse = await requestReturn.json();
+      this.setState({
+        isLoading: false,
+        lastDog: requestResponse
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  renderDog() {
+    const { lastDog } = this.state;
+    return (
+      <div>
+        <img src={lastDog.message} alt="" />
+        <button onClick={this.saveDog}>Nova foto</button>
+      </div>
+    );
+  }
+
+  saveDog() {
+    this.setState(({ listOfDogs, lastDog }) => ({
+      listOfDogs: [...listOfDogs, lastDog]
+    }))
+    this.fetchDog();
+  }
+
+  render() {
+    const { isLoading, listOfDogs } = this.state;
+    const loadingElement = <p>Loading...</p>
+    return (
+      <div className="App">
+        <span>
+          {listOfDogs.map(({ message }) => <img src={message} alt=""></img>)}
+        </span>
+        {isLoading ? loadingElement : this.renderDog()}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
